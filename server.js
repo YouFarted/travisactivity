f44d6938637d9ccf5238d6241e2226641fdf8867
Andrew Olson <frrrkin@gmail.com>
2021-01-20 20:27:07 -0800

    configured dotenv to be able to use the test database locally

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 // Requiring necessary npm packages
+require("dotenv").config();
 const express = require("express");
 const session = require("express-session");
 // Requiring passport as we've configured it
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 8080;
 const db = require("./models");
 
 // Creating express app and configuring middleware needed for authentication
+
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
