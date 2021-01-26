27ff4a8a6dceee081e8aeb65a9ba1d6809478793
Marco Bejarano <marco.bejarano03@gmail.com>
2021-01-25 20:28:17 -0800

    fixed the sign-up button for homepage.js

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,11 +1,18 @@
 $(document).ready(() => {
   const login = $("button#login");
-  //   const signup = $("button#signup");
+  const signup = $("button#signup");
   //   const searchButton = $("submit#search-button");
   //   const searchField = $("input#search-field");
 
+  // When login button is clicked USER is redirected to login page
   login.click(event => {
     event.preventDefault();
     window.location.href = "/login";
+  });
+
+// When signup button is clicked USER is redirected to signup page
+  signup.click(event => {
+    event.preventDefault();
+    window.location.href = "/signup";
   });
 });
