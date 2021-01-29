3b1d3ffcf0556304c8ea61d79e0a49180159920f
Andrew Olson <frrrkin@gmail.com>
2021-01-29 03:50:45 -0800

    ran locally on a Linux box and found failures.  Perhaps this'll fix the Heroku database reseeding failures.

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,7 +2,7 @@
 -- in a connection alreading using it.
 /* not finished */
 
-INSERT INTO users
+INSERT INTO Users
 (username, email, aboutMe, firstName, lastName, age, gender, hobbies, password)
 values('SomeGuy1', 'someguy1@gmail.com', 'I do things', 'Some', 'Guy', 20, 'Male', 'doing stuff', 'password1234'),
 ('SomeGal1', 'somegal1@gmail.com', 'I do more things', 'Some', 'Gal', 22, 'Female', 'doing all stuff', 'password555'),
@@ -10,11 +10,11 @@ values('SomeGuy1', 'someguy1@gmail.com', 'I do things', 'Some', 'Guy', 20, 'Male
 
 -- The password for yetanotherguy is a salted&encrypted 'password1234'
 
-INSERT INTO users
+INSERT INTO Users
 (username, isDeveloper, email, aboutMe, firstName, lastName, age, gender, hobbies, password)
 values('yetanotherguy', 1, 'yetanotherguy@gmail.com', 'blahblahblah', 'Another', 'Guy', 1234, 'Male', 'picking my bellybutton and sniffing it', '$2a$10$hu2/j4Iyyp7MTs52B3XuVO3TzujkeJFjPSy8YFklChaejUJc1YYJ6');
 
-INSERT INTO messages
+INSERT INTO Messages
 (subject, body, sendingUser_id, receivingUser_id)
 values('subject1', 'This is the first message', 'SomeGuy1', 'SomeGal1'),
 ('subject2', 'This is the second message', 'SomeGuy1', 'SomeGal1'),
@@ -22,13 +22,13 @@ values('subject1', 'This is the first message', 'SomeGuy1', 'SomeGal1'),
 ('subject4', 'This is the fourth message', 'SomeGuy1', 'SomeGal1'),
 ('subject5', 'This is the fourth message', 'SomeGuy1', 'SomeGal2');
 
-INSERT INTO addresses
+INSERT INTO Addresses
 (street, city, state, postalCode, user_id)
 values('111','Seattle', 'WA', '98118', 'SomeGuy1'),
 ('222','Seattle', 'WA', '98119', 'SomeGal1'),
 ('333','Everett', 'WA', '98120', 'SomeGal2');
 
-INSERT INTO photos
+INSERT INTO Photos
 (description, attachment, user_id)
 values('doing things', 'iAmABlob1', 'SomeGuy1'),
 ('doing things too', 'iAmABlob2', 'SomeGuy1'),
