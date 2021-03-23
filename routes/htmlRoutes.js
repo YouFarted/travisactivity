891234c91c6edc0d91bc39f6a5632c094a3985a0
Marco Bejarano <marco.bejarano03@gmail.com>
2021-03-22 18:56:11 -0700

    moved messages to members pages; also styled a bit

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -40,15 +40,33 @@ router.get("/login", (req, res) => {
 // Here we've add our isAuthenticated middleware to this route.
 // If a user who is not logged in tries to access this route they will be redirected to the signup page
 router.get("/members", isAuthenticated, (req, res) => {
-  if (
-    req.user.isDeveloper &&
-    !req.session.hasAlreadyBeenRedirectedFromMembersToDevelopersOnce
-  ) {
-    req.session.hasAlreadyBeenRedirectedFromMembersToDevelopersOnce = true;
-    res.redirect("/developers");
-  } else {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
-  }
+  const messageUsername = req.user.username;
+  const usernameToLookAt = req.user.username; 
+
+  console.log(`The url wants a user named ${messageUsername}`);
+  db.Message.findAll({
+    where: {
+      [Op.or]: [
+        { sendingUserId: usernameToLookAt },
+        { receivingUserId: usernameToLookAt }
+      ]
+    },
+    order: [["id", "DESC"]]
+  }).then(dbAllMyMessages => {
+    //We need to change the date field and store it back in the variable before
+    //rending in handlebars
+    const messagePostDateFormat = dbAllMyMessages.map(result => ({
+      createdAt: moment(result.createdAt).format("LLL"),
+      subject: result.subject,
+      sendingUserId: result.sendingUserId,
+      body: result.body,
+      sendingUserId: result.sendingUserId,
+      receivingUserId: result.receivingUserId
+    }));
+    console.log(messagePostDateFormat);
+    const message = { Message: messagePostDateFormat };
+    res.render("messages1", message);
+  });
 });
 
 //-----
@@ -94,31 +112,7 @@ router.get("/my-messages/:username", isAuthenticated, (req, res) => {
       .status(404)
       .json({ error: "you need to be logged in to see any profiles." });
   } else {
-    const messageUsername = req.params.username;
-    console.log(`The url wants a user named ${messageUsername}`);
-    db.Message.findAll({
-      where: {
-        [Op.or]: [
-          { sendingUserId: usernameToLookAt },
-          { receivingUserId: usernameToLookAt }
-        ]
-      },
-      order: [["id", "DESC"]]
-    }).then(dbAllMyMessages => {
-      //We need to change the date field and store it back in the variable before
-      //rending in handlebars
-      const messagePostDateFormat = dbAllMyMessages.map(result => ({
-        createdAt: moment(result.createdAt).format("LLL"),
-        subject: result.subject,
-        sendingUserId: result.sendingUserId,
-        body: result.body,
-        sendingUserId: result.sendingUserId,
-        receivingUserId: result.receivingUserId
-      }));
-      console.log(messagePostDateFormat);
-      const message = { Message: messagePostDateFormat };
-      res.render("messages1", message);
-    });
+    
   }
 });
 
